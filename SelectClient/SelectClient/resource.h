6348e038ff2064f69bf37c2751dc9b5a5c64f00a
DESKTOP-3RFQDQR\ParkMinHyun <algusl92@naver.com>
2017-05-30 02:45:25 +0900

    Select Model Client 초안

---
+++ SelectClient/SelectClient/resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Resource.rc에서 사용되고 있습니다.
+//
+#define IDD_DIALOG1                     101
+#define IDC_ISIPV6                      1001
+#define IDC_IPADDR                      1002
+#define IDC_PORT                        1003
+#define IDC_CONNECT                     1004
+#define IDC_SENDMSG                     1005
+#define IDC_MSG                         1006
+#define IDC_STATUS                      1007
+#define IDC_COLORRED                    1008
+#define IDC_COLORGREEN                  1009
+#define IDC_COLORBLUE                   1010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
